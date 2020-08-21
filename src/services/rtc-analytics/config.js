78e2297a7840c03e6d54e68b2486f2ac4a0c6afe
suqingxia <suqingxia@picooc.com>
2020-08-21 12:26:36 +0800

    #root overflow-y: auto

--- src/services/rtc-analytics/config.js
+++ src/services/rtc-analytics/config.js
@@ -1,18 +1,15 @@
 
 
-
-
-// const origin = '/api';
-// const origin = 'https://rtc-turn4-hsb.easemob.com';
-let origin = 'https://a1-hsb.easemob.com';
-// const origin = 'https://a1.easemob.com';
-
-if(sessionStorage.getItem('easemob-cluster') == 'vip6') {
-    origin = 'https://a1-vip6.easemob.com'
-}
-
-
 const get_prefix = () => { // 在调用API的时候，再去获取, 初始化不准确
+    // const origin = '/api';
+    // const origin = 'https://rtc-turn4-hsb.easemob.com';
+    let origin = 'https://a1-hsb.easemob.com';
+    // const origin = 'https://a1.easemob.com';
+
+    if(sessionStorage.getItem('easemob-cluster') == 'vip6') {
+        origin = 'https://a1-vip6.easemob.com'
+    }
+    
     const appkey = sessionStorage.getItem('easemob-appkey');
 
     let orgName, appName, prefix;
